3d90ae2a7f0c323d8f7ab4e376c2aa5361f9e8b2
Küng Marius (s) <marius.kueng@students.fhnw.ch>
2016-03-02 20:58:27 +0100

    Added city constructor

--- RoutePlannerLib/City.cs
+++ RoutePlannerLib/City.cs
@@ -13,5 +13,13 @@ namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib
         public int Population { get; set; }
         public WayPoint Location { get; set; }
 
+        public City(String Name, String Country, int Population, WayPoint Location)
+        {
+            this.Name = Name;
+            this.Country = Country;
+            this.Population = Population;
+            this.Location = Location;
+
+        }
     }
 }
