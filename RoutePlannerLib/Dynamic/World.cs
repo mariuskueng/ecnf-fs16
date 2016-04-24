5e67aa331198038efab8193c6dd44c8a4e72ecd5
Küng Marius (s) <marius.kueng@students.fhnw.ch>
2016-04-24 13:38:05 +0200

    Added dynamic class World

---
+++ RoutePlannerLib/Dynamic/World.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Dynamic;
+
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib.Dynamic
+{
+    public class World : DynamicObject
+    {
+        private Cities cities;
+        public World(Cities c)
+        {
+            cities = c;
+        }
+
+        public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args,
+                out object result)
+        {
+            try
+            {
+                City city = cities[binder.Name];
+                result = city;
+            }
+            catch (KeyNotFoundException)
+            {
+
+                result = $"The city \"{binder.Name}\" does not exist!";
+            }
+
+            return true;
+        }
+    }
+}
