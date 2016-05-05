b37b607a72e243ef4752170307e25d6695f569a1
Küng Marius (s) <marius.kueng@students.fhnw.ch>
2016-05-05 11:27:56 +0200

    Cleaned console app main method

--- RoutePlannerConsole/RoutePlannerConsoleApp.cs
+++ RoutePlannerConsole/RoutePlannerConsoleApp.cs
@@ -14,26 +14,6 @@ namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerConsole
         {
             Console.WriteLine("Welcome to Routeplanner (Version {0})", Assembly.GetExecutingAssembly().GetName().Version);
 
-            var windisch = new WayPoint("Windisch", 47.479319847061966, 8.212966918945312);
-            var bern = new WayPoint("Bern", 46.9479739, 7.4474468);
-            var tripolis = new WayPoint("Tripolis", 32.8872094, 13.1913383);
-            var rio = new WayPoint("Rio de Janeiro", -22.908970, -43.175922);
-
-            Console.WriteLine(windisch);
-            Console.WriteLine(bern.Distance(tripolis));
-
-            var cities = new Cities();
-            cities.ReadCities("data/citiesTestDataLab4.txt");
-
-            var routes = new Routes(cities);
-            var count = routes.ReadRoutes("data/linksTestDataLab4.txt");
-            var links = routes.FindShortestRouteBetween("Zürich", "Basel", TransportMode.Rail);
-
-            foreach (var link in links)
-            {
-                Console.WriteLine($"{link.FromCity.Name} - {link.ToCity.Name}");
-            }
-
             Console.WriteLine("Press any key to quit");
             Console.ReadKey();
         }
